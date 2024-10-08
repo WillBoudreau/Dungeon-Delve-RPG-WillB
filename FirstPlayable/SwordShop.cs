eb2042d000edd6f3e2aa1f6ac0e99c7a01938880
WillBoudreau <williamboudreau804@gmail.com>
2024-10-08 08:10:20 -0400

    Implementing the second shop

---
+++ FirstPlayable/SwordShop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPlayable
+{
+    internal class SwordShop:Shop
+    {
+        public SwordShop()
+        {
+            
+        }
+        public override void EnterShop(Player player)
+        {
+
+        }
+        public override void Buy(Player player,Item item)
+        {
+
+        }
+        public override void LeaveShop(Player player)
+        {
+
+        }
+    }
+}
